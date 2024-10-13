e0cd2c1152e2ec4ad957ab4bca0dcaaad1a0b714
Anik720 <heanik39@gmail.com>
2024-10-13 22:57:56 +0600

    finished the task

--- auth.js
+++ auth.js
@@ -1,16 +1,16 @@
-// auth.js
+
 const jwt = require('jsonwebtoken');
 
 const SECRET_KEY = 'secret_key'; 
 const payload = { userId: 'user_id' };
 const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1y' });
-console.log(token);
+
 
 function authenticate(token) {
   if (token) {
     try {
       const decoded = jwt.verify(token.replace('Bearer ', ''), SECRET_KEY);
-      return decoded; // Return user data if token is valid
+      return decoded; 
     } catch (err) {
       throw new Error('Invalid token');
     }
